1113b6962573e1fe52f469473ab9693e9f7b3ebf
Damien Seguin <dmnsgn@gmail.com>
2024-01-27 17:08:20 +0000

    build: update dependencies

--- web_modules/clamp.js
+++ web_modules/clamp.js
@@ -1,7 +1,9 @@
+import { g as getDefaultExportFromCjs } from './_chunks/_commonjsHelpers-jjO7Zipk.js';
+
 var clamp_1 = clamp;
-
 function clamp(value, min, max) {
-  return min < max ? value < min ? min : value > max ? max : value : value < max ? max : value > min ? min : value;
+    return min < max ? value < min ? min : value > max ? max : value : value < max ? max : value > min ? min : value;
 }
+var index = /*@__PURE__*/ getDefaultExportFromCjs(clamp_1);
 
-export default clamp_1;
+export { index as default };
